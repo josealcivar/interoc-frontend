ec7d310d560a5059d2c86233962690cadc32d521
JoseAndre <JoseAndre@DESKTOP-CFGHQSK.(none)>
2020-07-14 03:29:26 -0500

    actualizando dist to deploy

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '../dist/interoc-frontend'));
+app.use(express.static(__dirname + '../dist/'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '../dist/interoc-frontend/index.html'));
+    res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8089);
