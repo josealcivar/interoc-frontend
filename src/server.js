14dc12d7de1d9d589781ab41cfdb0e7c8cfca65a
JoseAndre <JoseAndre@DESKTOP-CFGHQSK.(none)>
2020-07-14 02:01:42 -0500

    upload deploy travis

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ng-blog'));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+app.use(express.static(__dirname + '/dist/interoc-frontend'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/interoc-frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8089);
